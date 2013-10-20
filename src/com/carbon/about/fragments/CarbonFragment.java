0d090d2b7323b86c7e660632882641b38146ba76
Daniel MM <daxx4182@gmail.com>
2013-10-20 22:42:40 +0800

    Second changes, ability build, cleanup

---
+++ src/com/carbon/about/fragments/CarbonFragment.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 Crossbones Software
+ * This code has been modified.  Portions copyright (C) 2013, Dirty Unicorns Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.about.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.carbon.about.R;
+import com.carbon.about.Utils;
+
+
+public class CarbonFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View carbonView = inflater.inflate(R.layout.carbon_fragment, container, false);
+
+        TextView carbonVersion = (TextView) carbonView.findViewById(R.id.carbon_version);
+        String version =  Utils.getCarbonVersion();
+        carbonVersion.append(" ");
+
+        // Inflate the layout for this fragment
+        return carbonView;
+    }
+}
